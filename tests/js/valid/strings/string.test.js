2f924ff747a6c91bec2dd236f7a27993ce168424
Aaron Stanek <amr.stanek@gmail.com>
2023-02-02 22:27:38 +0000

    Test hello string

--- tests/js/valid/strings/string.test.js
+++ tests/js/valid/strings/string.test.js
@@ -1,7 +1,11 @@
-import { emptyString } from "@tests/js/valid/strings/string.mjs"
+import { helloString, emptyString } from "@tests/js/valid/strings/string.mjs"
 
 import { expect, it } from "bun:test"
 
 it("empty string literal should have the value of an empty string", () => {
     expect(emptyString.valueOf()).toBe("")
+})
+
+it("hello string literal should have the value of hello", () => {
+    expect(helloString.valueOf()).toBe("hello")
 })
