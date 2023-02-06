8d0a79b76899bb0eb77e66caed12492cdae2be99
Aaron Stanek <amr.stanek@gmail.com>
2023-02-06 23:30:56 +0000

    scope

---
+++ rust/type_checker/src/scope.rs
@@ -0,0 +1,16 @@
+use crate::{type_schema::TypeSchema, GenericTypeId};
+use std::collections::HashMap;
+
+pub struct Scope<'a, 'b> {
+    pub parent: Option<&'a Scope<'b, 'b>>,
+    pub identifiers: HashMap<String, GenericTypeId>,
+}
+
+impl<'a, 'b> Scope<'a, 'b> {
+    pub fn get_declaration_type(&self, variable_name: &str) -> Option<GenericTypeId> {
+        self.identifiers.get(variable_name).map_or_else(
+            || self.parent.and_then(|parent| (*parent).get_declaration_type(variable_name)),
+            |x| Some(*x),
+        )
+    }
+}
