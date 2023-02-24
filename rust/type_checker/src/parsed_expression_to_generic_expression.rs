73ece4524b4796c665263718704e65dd4d533bf3
Aaron Stanek <amr.stanek@gmail.com>
2023-02-24 01:42:17 +0000

    translate_binary_operator_add_arithmetic_constraints

--- rust/type_checker/src/parsed_expression_to_generic_expression.rs
+++ rust/type_checker/src/parsed_expression_to_generic_expression.rs
@@ -69,10 +69,18 @@ fn translate_binary_operator_add_arithmetic_constraints(
     schema: &mut TypeSchema,
     id_collection: &TranslateBinaryOperatorIdCollection,
 ) -> Result<(), String> {
-    schema.add_constraint(id_collection.type_id, constrain_equal_to_num())?;
-    schema.add_constraint(id_collection.left_child_id, constrain_equal_to_num())?;
-    schema.add_constraint(id_collection.right_child_id, constrain_equal_to_num())?;
-    Ok(())
+    match schema
+        .add_constraint(id_collection.type_id, constrain_equal_to_num())
+        .and_then(|_| schema.add_constraint(id_collection.left_child_id, constrain_equal_to_num()))
+        .and_then(|_| schema.add_constraint(id_collection.right_child_id, constrain_equal_to_num()))
+    {
+        Ok(x) => Ok(x),
+        Err(error) => {
+            let mut message = "TranslateBinaryOperatorAddArithmeticConstraints: ".to_owned();
+            message.push_str(error.as_str());
+            Err(message)
+        }
+    }
 }
 
 fn translate_binary_operator_add_concatenate_constraints(
