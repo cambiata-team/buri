74b68bdacec2959732c7f7cb672ffd3590222878
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-06-25 11:01:00 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -4,20 +4,29 @@ set -e
 
 ARCH=`uname -m`
 OS=`uname`
-TARGET="buri_${OS}_${ARCH}"
+FILENAME="buri_${OS}_${ARCH}"
+EXTENSION=""
 
 if [ "${OS}" = "Windows" ]
 then
-    TARGET="${TARGET}.zip"
+    EXTENSION=".zip"
 else
-    TARGET="${TARGET}.tar.gz"
+    EXTENSION=".tar.gz"
 fi
 
 URL="https://github.com/cambiata-team/buri-go/releases/latest/download/"
-URL="${URL}${TARGET}"
+URL="${URL}${FILENAME}${EXTENSION}"
 
 echo "Downloading buri... $URL"
 curl --fail --location --progress-bar --output /usr/local/bin/buri $URL
+if [ "${OS}" == "Windows" ]
+then
+    unzip -a /usr/local/bin/buri
+else
+    tar -xvf /usr/local/bin/buri
+fi
+rm /usr/local/bin/buri
+mv buri /usr/local/bin/buri
 chmod +x /usr/local/bin/buri
 echo "buri was successfully installed to /usr/local/bin/buri"
 echo "Run \`bkg --help\` to get started!"
