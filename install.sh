406bfdf0b4b804964d9601df4357378c82ad22bd
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-07-01 16:50:11 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -4,18 +4,32 @@ set -e
 
 ARCH=`uname -m`
 OS=`uname`
-FILENAME="buri_${OS}_${ARCH}"
-EXTENSION=""
+FILENAME="cli-"
 
-if [ "${OS}" = "Windows" ]
+if [ "${ARCH}" == "arm64" ]
 then
-    EXTENSION=".zip"
+    FILENAME="${FILENAME}aarch64-"
+elif [ "${ARCH}" == "x86_64" ]
+then
+    FILENAME="${FILENAME}x86_64-"
 else
-    EXTENSION=".tar.gz"
+    echo "Unsupported architecture: ${ARCH}"
+    exit 1
+fi
+
+if [ "${OS}" == "Darwin" ]
+then
+    FILENAME="${FILENAME}apple-darwin"
+elif [ "${OS}" == "Linux" ]
+then
+    FILENAME="${FILENAME}unknown-linux-gnu"
+else
+    echo "Unsupported OS: ${OS}"
+    exit 1
 fi
 
 URL="https://github.com/cambiata-team/buri-go/releases/latest/download/"
-URL="${URL}${FILENAME}${EXTENSION}"
+URL="${URL}${FILENAME}.tar.gz"
 
 echo "Downloading buri... $URL"
 curl --fail --location --progress-bar --output /usr/local/bin/buri-tmp $URL
@@ -27,6 +41,7 @@ else
     tar -xvf buri-tmp
 fi
 rm /usr/local/bin/buri-tmp
+mv /usr/local/bin/cli /usr/local/bin/buri
 chmod +x /usr/local/bin/buri
 echo "buri was successfully installed to /usr/local/bin/buri"
-echo "Run \`bkg --help\` to get started!"
+echo "Run \`buri --help\` to get started!"
