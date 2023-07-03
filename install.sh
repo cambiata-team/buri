fb5830d8500836d2583f0479827f557b2be4c1dc
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-07-03 11:52:14 -0700

    Store CLI version

--- install.sh
+++ install.sh
@@ -32,9 +32,15 @@ URL="https://github.com/cambiata-team/buri-go/releases/latest/download/"
 URL="${URL}${FILENAME}.tar.gz"
 
 echo "Downloading buri... $URL"
+
 TEMP_FILE=$(mktemp)
 TEMP_DIRECTORY=$(mktemp -d)
 BURI_LOCATION=/usr/local/bin/buri
+CONFIG_DIRECTORY=~/.config/buri
+VERSION_URL=https://version-api.buri-lang.dev/get-latest-cli-version-plaintext
+
+mkdir -p $CONFIG_DIRECTORY
+curl --fail --location $CONFIG_DIRECTORY/cli-version $VERSION_URL
 curl --fail --location --progress-bar --output $TEMP_FILE $URL
 tar -xvf $TEMP_FILE -C $TEMP_DIRECTORY
 mv $TEMP_DIRECTORY/cli $BURI_LOCATION
