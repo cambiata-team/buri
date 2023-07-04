354fc4ce3ca364dc150dd9a6c3847b91e523eef3
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2023-07-04 13:16:25 -0700

    Add encoding/decoding tests

--- protos/src/lib.rs
+++ protos/src/lib.rs
@@ -31,3 +31,41 @@ where
 pub fn decode_base_64_to_bytes(base_64: &str) -> Vec<u8> {
     ENGINE.decode(base_64).unwrap()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn encoding_and_decoding_from_base_64_gets_same_message() {
+        let message = version::GetVersionDownloadInfoResponse {
+            version_number: "1.0.0".to_string(),
+            download_urls: vec!["https://example.com".to_string()],
+            checksum: Some(version::Checksum {
+                hash_function: version::HashFunction::Sha256 as i32,
+                checksum: "1234567890".to_string(),
+            }),
+        };
+        let encoded = encode_message_to_base_64(&message);
+        let decoded = decode_base_64_to_bytes(&encoded);
+        let decoded_message =
+            version::GetVersionDownloadInfoResponse::decode(decoded.as_slice()).unwrap();
+        assert_eq!(message, decoded_message);
+    }
+
+    #[test]
+    fn encoding_and_decoding_from_bytes_gets_same_message() {
+        let message = version::GetVersionDownloadInfoResponse {
+            version_number: "1.0.0".to_string(),
+            download_urls: vec!["https://example.com".to_string()],
+            checksum: Some(version::Checksum {
+                hash_function: version::HashFunction::Sha256 as i32,
+                checksum: "1234567890".to_string(),
+            }),
+        };
+        let encoded = encode_message_to_bytes(&message);
+        let decoded_message =
+            version::GetVersionDownloadInfoResponse::decode(encoded.as_slice()).unwrap();
+        assert_eq!(message, decoded_message);
+    }
+}
